f407531125962f8378a4742f8b513b3cc70afc26
allxdxd <43080946+allxdxd@users.noreply.github.com>
2021-07-04 17:45:01 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+const bedrock = require('bedrock-protocol')
+const http = require('http');
+
+http.createServer((req, res)=>{
+  res.write('Funcionando');
+  res.end;
+}).listen(process.env.PORT || 5000)
+
+const client = bedrock.createClient({
+  host: 'ab01.aternos.me',  
+  port: 60526,        
+  username: 'Bob',
+  offline: true
+});
+
+client.on('join', client => console.log('Player has joined!'))
+
+client.on('join', client => console.log('Player has spawned!'))
+
+client.on('text', (packet) => {
+  console.log( packet.source_name, ": " , packet.message)
+})
