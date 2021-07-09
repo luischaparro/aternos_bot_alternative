98bbddd5b4be45d25cfb97124fce3ad22e5eb8fd
allxdxd <43080946+allxdxd@users.noreply.github.com>
2021-07-09 17:49:15 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,15 +1,29 @@
-const bedrock = require('bedrock-protocol')
-const http = require('http');
+const bedrock = require('bedrock-protocol');
+const http = require("http");
+const ping = require('node-http-ping');
 
-http.createServer((req, res)=>{
-  res.write('Funcionando');
-  res.end;
-}).listen(process.env.PORT || 5000)
+/*Crear servidor web que establezca un puerto*/
 
+const server = http.createServer((req, res) => {
+  console.log("On");
+  res.end();
+}).listen(process.env.PORT);
+
+/*Hacer ping para simular trafico*/
+
+function trafico(){
+  ping('https://aternos-bot-alternative.herokuapp.com')
+  .then(time => console.log(`Response time: ${time}ms`))
+  .catch(() => console.log('Failed to ping'))
+};
+
+setInterval(trafico, 3000);
+
+/*Crear jugador y hacer que entre*/
 const client = bedrock.createClient({
   host: 'ab01.aternos.me',  
   port: 60526,        
-  username: 'Sam',
+  username: 'Bob',
   offline: true
 });
 
@@ -19,4 +33,4 @@ client.on('join', client => console.log('Player has spawned!'))
 
 client.on('text', (packet) => {
   console.log( packet.source_name, ": " , packet.message)
-})
+});
